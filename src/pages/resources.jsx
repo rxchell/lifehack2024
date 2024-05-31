35408b222f90962e8b11c4ae4a09a0c63d7cfbfe
rxchell <e0969575@u.nus.edu>
2024-06-01 00:51:39 +0800

    resources page with websites and helpline

--- src/pages/resources.jsx
+++ src/pages/resources.jsx
@@ -2,6 +2,8 @@ import Header from '../components/header';
 import Chatbot from '../components/chatbot';
 import resource from '../data/resource.json';
 import ResourceCard from '../components/resource_cards';
+import website from '../data/website.json';
+import WebsiteCard from '../components/website_cards';
 import './style/pages.css';
 
 function ResourcesPage() {
@@ -18,6 +20,16 @@ function ResourcesPage() {
 
             <br />
             <h1 className='pages-title'>Useful Websites</h1>
+            <div className='quiz-cards'>
+                {website.map((websiteItem) => (
+                    <WebsiteCard key={websiteItem.url} data={websiteItem} />
+                ))}
+            </div>
+
+            <br />
+            <h1 className='pages-title'>Helpline</h1>
+            <p className='helpline-description'>Anti-Scam Helpline: 1800-722-6688 (Mon-Fri: 9am to 6pm, excluding Public Holidays)</p>
+            <p className='report-description'>Report a scam: https://form.gov.sg/63982e109841390011a5912</p>
         </div>
     )
 }
