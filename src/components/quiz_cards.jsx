07f06698d4367c12e73f0a85763e165945b60ca3
tahnya <111273056+tahnya@users.noreply.github.com>
2024-05-31 15:49:46 +0800

    Add quiz cards
    Add cards from data
    Style cards

--- src/components/quiz_cards.jsx
+++ src/components/quiz_cards.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import './style/quiz_cards.css';
+
 function QuizCard (props) {
     const navigate = useNavigate();
 
@@ -5,6 +9,10 @@ function QuizCard (props) {
 
     return (
         <div className="quiz-card">
+            <p className="quiz-card-title">{data.title}</p>
+            <p className="quiz-card-description">{data.description}</p>
         </div>
     )
 }
+
+export default QuizCard;
