0e1d54fc6f50da9e0953548795f5f42586b2b181
yapxuanxuan <e0969213@u.nus.edu>
2024-06-01 10:24:55 +0800

    Removed changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,10 +16,7 @@ function Home() {
 
             <div className="home-content">
                 <h1 className="pages-title">Home</h1>
-                <p className="home-description"> ScamSense is here to help you be more aware of the ways
-                scammers try to target you. Let's combat financial frauds together and stay safe from scams!
-                <br></br><br></br> Try out the following quizzes below to find out your ability at tackling financial frauds!
-                </p>
+                <p className="home-description">Let's combat financial frauds!</p>
 
                 <div className="quiz-cards">
                     {quiz.map((quizItem) => (
